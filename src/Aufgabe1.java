ed1b5dc6c0320729e5e79a593143a160d3d5db14
lukas-haindl <lukas.haindl03@icloud.com>
2024-11-26 14:41:42 +0100

    complete Aufgabe1

--- src/Aufgabe1.java
+++ src/Aufgabe1.java
@@ -4,7 +4,7 @@
 public class Aufgabe1 {
 
     private static void printArray(int[] workArray) {
-        for (int i = workArray.length; i >= 0; i--) {
+        for (int i = workArray.length; i > 0; i--) {
             System.out.print(workArray[i-1] + " ");
         }
         System.out.println();
@@ -58,11 +58,19 @@ public class Aufgabe1 {
     //**** Notizen und Fragebeantwortungen bitte hier unterhalb durchführen! ***
     //**************************************************************************
     //Antwort zu Punkt a:
-    //
+    //Das Erste Element in einem Array hat Index 0, durch dass i >= 0, wird im letzten Schleifendurchlauf versucht auf das Element auf Index -1 zuzugreifen, welchen es nicht gibt
     //Antwort zu Punkt b:
-    //
+    //Weil ein Array nur eine Referenz ist und diese mitgegeben wird, wird direkt, dass Original Array verändert.
     //Antwort zu Punkt c:
-    //
+    //Es wird keine echte Kopie erstellt, sondern die Referenz aufs ursprüngliche Array gesetzt.
     //Antwort zu Punkt d:
-    //
+    //Das workArray in der Methode ist nur eine lokale Referenz, daher wirken sich Änderungen nicht aufs Original aus.
+    //Antwort zu Zusatzfrage 1:
+    //int, es würden aber auch short, byte und char funktionieren
+    //Antwort zu Zusatzfrage 2:
+    //Neues Array erstellen und die alten Werte kopieren
+    //Antwort zu Zusatzfrage 3:
+    //System.arraycopy(original, 0, copy, 0, original.length)
+    //Antwort zu Zusatzfrage 4:
+    //Nein, es wird die Referenzgleichheit geprüft.
 }
